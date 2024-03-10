2b102e40f1920f211423e516be3e9f42ef69d02d
Shahlar95 <asahlar3@gmail.com>
2024-03-10 15:28:36 +0400

    add/newproject/nav/footer

---
+++ Web conference/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Layout from './Layout/Layout'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Layout>
+      salam
+
+    </Layout>
+    </>
+  )
+}
+
+export default App
---
+++ Web conference/src/Layout/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div className="container">
+            <div>
+            <div className='logo'>New Event</div>
+            <div className='menu'>
+                <ul>
+                    <li>intro</li>
+                    <li>overview</li>
+                    <li>speakers</li>
+                    <li>programs</li>
+                    <li>register</li>
+                    <li>venue</li>
+                    <li>sponsors</li>
+                    <li>contact</li>
+                </ul>
+            </div>
+            </div>
+            
+        </div>
+    </header>
+  )
+}
+
+export default Header
