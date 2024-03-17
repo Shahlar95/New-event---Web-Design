01e35944af9e731262603695275eb4156384b866
Shahlar95 <asahlar3@gmail.com>
2024-03-17 12:29:47 +0400

    add/navbar/

--- Web conference/src/Layout/Header/Header.jsx
+++ Web conference/src/Layout/Header/Header.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './_Header.scss'
 
 function Header() {
   return (
     <header>
         <div className="container">
-            <div>
-            <div className='logo'>New Event</div>
+            <div className='nav'>
+            <div className='logo'>
+              <img src="https://codeskdhaka.com/wp/wetland/wp-content/themes/wetland/assets/img/logo/logo.png" alt="logo" />
+            </div>
             <div className='menu'>
                 <ul>
-                    <li>intro</li>
-                    <li>overview</li>
-                    <li>speakers</li>
-                    <li>programs</li>
-                    <li>register</li>
-                    <li>venue</li>
-                    <li>sponsors</li>
-                    <li>contact</li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Service</li>
+                    <li>Blog</li>
+                    <li>Pages</li>
+                    <li>Contact</li>
+                   
                 </ul>
+            </div>
+            <div className='login-signup'>
+                <div className='login'>Login</div>
+                <div className='signup'><button>Sign Up</button></div>
             </div>
             </div>
             
